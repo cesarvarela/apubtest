e575931cfcff3c356fe744a1c78589856f583f8b
Cesar Varela <mail@cesarvarela.com>
2025-07-21 20:41:23 -0300

    Mark problematic package as external

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['rdf-canonize-native']
 };
 
 export default nextConfig;
